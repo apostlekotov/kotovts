4c0fa6eb9326e28f432d2e71160d9c7574dcd3a9
Kotov <kotov.smtp@gmail.com>
2022-05-08 16:12:38 +0300

    🖼 assets added

---
+++ src/assets/img/index.tsx
@@ -0,0 +1,4 @@
+import LogoImage from "@/public/img/kotov-ebalo.png";
+import ProfileImage from "@/public/img/about.jpg";
+
+export { LogoImage, ProfileImage };
---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,5 @@
+import SunIcon from "@/public/svg/light.svg";
+import MoonIcon from "@/public/svg/dark.svg";
+import LanguageIcon from "@/public/svg/language.svg";
+
+export { SunIcon, MoonIcon, LanguageIcon };
