1e52e136c0418f5237ee33503c517cbb2c111b35
Kotov <kotov.smtp@gmail.com>
2022-05-09 17:45:51 +0300

    🖼 assets added

---
+++ src/assets/projects/index.tsx
@@ -0,0 +1,3 @@
+import Project1Image from "@/public/img/projects/the-mafia-helper.png";
+
+export { Project1Image };
--- src/assets/svg/index.tsx
+++ src/assets/svg/index.tsx
@@ -1,5 +1,19 @@
 import SunIcon from "@/public/svg/light.svg";
 import MoonIcon from "@/public/svg/dark.svg";
 import LanguageIcon from "@/public/svg/language.svg";
+import GithubIcon from "@/public/svg/github.svg";
+import TelegramIcon from "@/public/svg/telegram.svg";
+import LinkedInIcon from "@/public/svg/linkedin.svg";
+import InstagramIcon from "@/public/svg/instagram.svg";
+import Spinner from "@/public/svg/spinner.svg";
 
-export { SunIcon, MoonIcon, LanguageIcon };
+export {
+  SunIcon,
+  MoonIcon,
+  LanguageIcon,
+  GithubIcon,
+  LinkedInIcon,
+  TelegramIcon,
+  InstagramIcon,
+  Spinner
+};
