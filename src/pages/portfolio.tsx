b1415b499a24c6f2ff032042256a0bc08e60bd97
Kotov <kotov.smtp@gmail.com>
2022-05-12 21:02:20 +0300

    🌑 dark mode fixed

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -18,7 +18,7 @@ const Portfolio: NextPage = () => {
       </Head>
 
       <section className='relative lg:mt-24 py-32 px-8 overflow-x-clip'>
-        <div className='absolute inset-0 bottom-auto h-64 scale-y-125 scale-x-150 translate-x-1/4 rotate-6 bg-gradient-to-tr from-white dark:from-transparent to-primary/30 blur-3xl -z-10' />
+        <div className='absolute inset-0 bottom-auto h-64 scale-y-125 scale-x-150 translate-x-1/4 rotate-6 bg-gradient-to-tr from-white dark:from-transparent to-primary-500/30 dark:to-primary-400/30 blur-3xl -z-10' />
 
         <div className='max-w-screen-2xl w-full mx-auto'>
           <h1 className='text-4xl lg:text-6xl font-black mb-16'>
