0d89a01850e1817cb730e19d323df308478829b2
Kotov <kotov.smtp@gmail.com>
2022-05-13 22:14:27 +0300

    🖼 assets changed

--- src/assets/projects/index.tsx
+++ src/assets/projects/index.tsx
@@ -1,3 +1,10 @@
-import Project1Image from "@/public/img/projects/the-mafia-helper.png";
+import MafiaHelper from "@/public/img/projects/the-mafia-helper.png";
+import UNFT from "@/public/img/projects/unft.png";
+import Triplex from "@/public/img/projects/triplex.png";
+import Sedam from "@/public/img/projects/sedam.png";
+import Limity from "@/public/img/projects/limity.png";
+import SmartArt from "@/public/img/projects/smartart.png";
 
-export { Project1Image };
+const ProjectImages = { MafiaHelper, UNFT, Triplex, Sedam, Limity, SmartArt };
+
+export default ProjectImages;
